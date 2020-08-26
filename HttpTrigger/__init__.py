fe58ea46665cff981815c307cd5f6c72c7053a76
Darryl Woodford <darryl@dpwoodford.net>
2020-08-27 00:26:01 +1000

    More debug

--- HttpTrigger/__init__.py
+++ HttpTrigger/__init__.py
@@ -17,30 +17,33 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     driver = webdriver.Chrome("/usr/local/bin/chromedriver", chrome_options=chrome_options)
 
-    url = req.params.get('url')
-    width = req.params.get('size').split("-")[0]
-    height = req.params.get('size').split("-")[1]
+    try:
+        url = req.params.get('url')
+        width = int(req.params.get('size').split("-")[0])
+        height = int(req.params.get('size').split("-")[1])
+        driver.get(url)
+        # links = driver.find_elements_by_tag_name("a")
+        # link_list = ""
+        # for link in links:
+        #     if link_list == "":
+        #         link_list = link.text
+        #     else:
+        #         link_list = link_list + ", " + link.text
+        driver.set_window_size(width,height)
+        screenshot = driver.get_screenshot_as_png()
 
-    driver.get(url)
-    # links = driver.find_elements_by_tag_name("a")
-    # link_list = ""
-    # for link in links:
-    #     if link_list == "":
-    #         link_list = link.text
-    #     else:
-    #         link_list = link_list + ", " + link.text
-    driver.set_window_size(width,height)
-    screenshot = driver.get_screenshot_as_png()
+        # create blob service client and container client
+        credential = DefaultAzureCredential()
+        storage_account_url = "https://" + os.environ["par_storage_account_name"] + ".blob.core.windows.net"
+        client = BlobServiceClient(account_url=storage_account_url, credential=credential)
+        blob_name = url.split('azurewebsites.net/')[1].replace("/","-")+".png"
+        blob_client = client.get_blob_client(container=os.environ["par_storage_container_name"], blob=blob_name)
+        blob_client.upload_blob(screenshot)
 
-    # create blob service client and container client
-    credential = DefaultAzureCredential()
-    storage_account_url = "https://" + os.environ["par_storage_account_name"] + ".blob.core.windows.net"
-    client = BlobServiceClient(account_url=storage_account_url, credential=credential)
-    blob_name = url.split('azurewebsites.net/')[1].replace("/","-")+".png"
-    blob_client = client.get_blob_client(container=os.environ["par_storage_container_name"], blob=blob_name)
-    blob_client.upload_blob(screenshot)
-
-    return func.HttpResponse(
-             str(blob_name),
-             status_code=200
-    )
+        return func.HttpResponse(
+                str(blob_name),
+                status_code=200
+        )
+    except:
+        logging.info('Something went very very wrong')
+        raise
